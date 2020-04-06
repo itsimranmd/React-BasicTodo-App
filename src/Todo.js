090489ee939cfab9e613b8a0c39e6668bfeddc92
yeahthatnerd <itsimranmd@gmail.com>
2020-04-06 14:12:24 +0530

    Added functionality to delete , show the status of tasks and add the task

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default function ({todo}) {
-    return (
-   <div>
-      {todo.name}
-      </div> 
-    )
+export default function Todo({ todo, toggleTodo }) {
+  function handleTodoClick() {
+    toggleTodo(todo.id)
+  }
+  
+  return (
+    <div>
+      <label>
+        <input type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
+        {todo.name}
+      </label>
+    </div>
+  )
 }
